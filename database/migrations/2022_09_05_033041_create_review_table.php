48d40653dc1cdb88d1bca8b0e3eeaf66b5429fa9
aaronlinggo <aaronlinggosatria@gmail.com>
2022-09-06 15:20:54 +0700

    create migration for review

---
+++ database/migrations/2022_09_05_033041_create_review_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('review', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('service_id')->nullable()->index('fk_review_to_service');
+            $table->foreignId('users_id')->nullable()->index('fk_review_to_users');
+            $table->longText('comment')->nullable();
+            $table->integer('rating');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('review');
+    }
+};
