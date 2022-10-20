19f057a432b0c70fa5630bccc899973a8da3eb52
Acxel Derian <acxelderian@gmail.com>
2022-10-20 20:51:32 +0700

    make model for advantage user and advantage service

--- app/Models/AdvantageUser.php
+++ app/Models/AdvantageUser.php
@@ -4,8 +4,27 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class AdvantageUser extends Model
 {
-    use HasFactory;
+    // use HasFactory;
+    use SoftDeletes;
+
+    public $table = "advantage_user";
+
+
+    protected $dates = [
+        'updated_at',
+        'created_at',
+        'deleted_at'
+    ];
+
+    protected $fillable = [
+        'service_id',
+        'advantage',
+        'updated_at',
+        'created_at',
+        'deleted_at'
+    ];
 }
