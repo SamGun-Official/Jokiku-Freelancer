7bd86e5e2fdebbc12fbef3a51ac5c4c48783c0ea
Acxel Derian <acxelderian@gmail.com>
2022-10-20 20:53:47 +0700

    make model for tagline and tumbnail service

--- app/Models/ThumbnailService.php
+++ app/Models/ThumbnailService.php
@@ -4,8 +4,27 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class ThumbnailService extends Model
 {
-    use HasFactory;
+    // use HasFactory;
+    use SoftDeletes;
+
+    public $table = "thumbnail_service";
+
+
+    protected $dates = [
+        'updated_at',
+        'created_at',
+        'deleted_at'
+    ];
+
+    protected $fillable = [
+        'service_id',
+        'thumbnail',
+        'updated_at',
+        'created_at',
+        'deleted_at'
+    ];
 }
