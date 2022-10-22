e2140f3d50c6acbb1c0fcd4a40ae5de13ef24feb
aaronlinggo <aaronlinggosatria@gmail.com>
2022-10-22 22:04:14 +0700

    Slicing Page Edit Profile

--- app/Http/Controllers/Dashboard/ProfileController.php
+++ app/Http/Controllers/Dashboard/ProfileController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers\Dashboard;
 
-use App\Http\Controllers\Controller;
+use App\Models\User;
+use App\Models\DetailUser;
 use Illuminate\Http\Request;
-
+use App\Models\ExperienceUser;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Requests\Dashboard\Profile\UpdateProfileRequest;
+use App\Http\Requests\Dashboard\Profile\UpdateDetailUserRequest;
+use File;
 class ProfileController extends Controller
 {
     /**
@@ -14,7 +20,13 @@ class ProfileController extends Controller
      */
     public function index()
     {
-        //
+        $user = User::where('id', Auth::user()->id)->first();
+
+        $experience_user = ExperienceUser::where('detail_user_id', $user->detail_user->id)
+                        ->orderBy('id', 'asc')
+                        ->get();
+
+        return view('pages.dashboard.profile', compact('user', 'experience_user'));
     }
 
     /**
@@ -67,9 +79,67 @@ class ProfileController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(UpdateProfileRequest $request_profile, UpdateDetailUserRequest $request_detail_user)
     {
-        //
+        $data_profile = $request_profile->all();
+        $data_detail_user = $request_detail_user->all();
+
+        // get photo user
+        $get_photo = DetailUser::where('users_id', Auth::user()->id)->first();
+
+        // delete old file from storage
+        if (isset($data_detail_user['photo'])){
+            $data = 'storage/'.$get_photo['photo'];
+            if (File::exists($data)){
+                File::delete($data);
+            }else{
+                File::delete('storage/app/public/'.$get_photo['photo']);
+            }
+        }
+
+        // store file to storage
+        if(isset($data_detail_user['photo'])){
+            $data_detail_user['photo'] = $request_detail_user->file('photo')->store('assets/photo', 'public');
+        }
+
+
+        // proses save to user
+        $user = User::find(Auth::user()->id);
+
+        $user->update($data_profile);
+
+        // proses save to detail user
+        $detail_user = DetailUser::find($user->detail_user->id);
+
+        $detail_user->update($data_detail_user);
+
+        // proses save to experience
+        $experience_user_id = ExperienceUser::where('detail_user_id', $detail_user['id'])->first();
+
+        if(isset($experience_user_id)){
+
+            foreach ($data_profile['experience'] as $key => $value) {
+                $experience_user = ExperienceUser::find($key);
+                $experience_user->detail_user_id = $detail_user['id'];
+                $experience_user->experience = $value;
+                $experience_user->save();
+            }
+
+        }
+        else{
+            foreach ($data_profile['experience'] as $key => $value) {
+                if (isset($value)){
+                    $experience_user = new ExperienceUser;
+                    $experience_user->detail_user_id = $detail_user['id'];
+                    $experience_user->experience = $value;
+                    $experience_user->save();
+                }
+            }
+        }
+
+        toast()->success('Update has been success');
+
+        return back();
     }
 
     /**
@@ -85,6 +155,24 @@ class ProfileController extends Controller
 
     // custom
     public function delete(){
+        // get user
+        $get_user_photo = DetailUser::where('users_id', Auth::user()->id)->first();
+        $path_photo = $get_user_photo['photo'];
 
+        // second update value to null
+        $data = DetailUser::find($get_user_photo['id']);
+        $data->photo = NULL;
+        $data->save();
+
+        // delete file photo
+        $data = 'storage/'.$path_photo;
+        if(File::exists($data)){
+            File::delete($data);
+        }else{
+            File::delete('storage/app/public/'.$path_photo);
+        }
+
+        toast()->success('Delete has been success');
+        return back();
     }
 }
