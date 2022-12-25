bd78f4be5861b86e249f43ebf0391587acf7b33a
PheyZhang <106577843+PheyZhang@users.noreply.github.com>
2022-12-25 23:00:05 +0700

    Update dekstop.blade.php

--- resources/views/component/Dashboard/dekstop.blade.php
+++ resources/views/component/Dashboard/dekstop.blade.php
@@ -307,14 +307,14 @@
                 </li>
                 <li class="relative px-6 py-3">
                             @if (request()->is('admin/user') ||
-                    request()->is('admin/user/*') ||
-                    request()->is('admin/*/user') ||
-                    request()->is('admin/*/user/*'))
+                                request()->is('admin/user/*') ||
+                                request()->is('admin/*/user') ||
+                                request()->is('admin/*/user/*'))
                     <span class="absolute inset-y-0 left-0 w-1 rounded-tr-lg rounded-br-lg bg-serv-bg"
                         aria-hidden="true"></span>
                     @endif
                     <a class="inline-flex items-center w-full text-sm font-light transition-colors duration-150 hover:text-gray-800"
-                    href="{{ route('member.profile.index') }}">
+                    href="{{ route('admin.profile.index') }}">
                     <svg width="24" height="24" viewBox="0 0 24 24" fill="none"
                         xmlns="http://www.w3.org/2000/svg">
                         <rect width="24" height="24" fill="white" />
